c561001ceef040faf480aa27e44bf0ec19390ab1
Thuyen Code <thuyencode@proton.me>
2024-01-20 22:25:35 +0700

    A minor UI change

--- src/components/cards/ForecastDay.tsx
+++ src/components/cards/ForecastDay.tsx
@@ -4,7 +4,7 @@ import { type ForecastDay as IForecastDay } from '../../libs/types'
 
 const ForecastDay: Component<IForecastDay> = (props) => {
   return (
-    <div class='card flex min-h-36 max-w-32 flex-col justify-between rounded-md border border-base-content/50 bg-base-100 px-3 py-2 text-center text-base-content'>
+    <div class='card flex min-h-36 min-w-20 flex-col justify-between rounded-md border border-base-content/50 bg-base-100 px-3 py-2 text-center text-base-content'>
       <span class='uppercase'>{WEEK_DAYS[new Date(props.date).getDay()]}</span>
       <img
         class='aspect-auto h-auto min-w-14'
--- src/components/skeletons/ForecastDaySkeleton.tsx
+++ src/components/skeletons/ForecastDaySkeleton.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const ForecastDaySkeleton: Component<Props> = (props) => {
   return (
-    <div class='card h-min max-w-32 rounded-md border border-base-content/50 bg-base-100 p-3 pt-2 text-center text-base-content'>
+    <div class='card h-min min-h-36 min-w-20 rounded-md border border-base-content/50 bg-base-100 p-3 pt-2 text-center text-base-content'>
       <span class='uppercase'>{props.weekDay}</span>
       <div class='skeleton my-3 size-10 rounded-full' />
       <div class='skeleton mt-1 h-5 w-full' />
